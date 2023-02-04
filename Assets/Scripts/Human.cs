8de8ce20ddd79fcdd45e9ffd520a300587ffeca1
Rarrix <32179106+Rarrix@users.noreply.github.com>
2023-02-04 21:14:17 +0200

    Add knights, postprocessing and enable deferred rendering

--- Assets/Scripts/Human.cs
+++ Assets/Scripts/Human.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class Human : MonoBehaviour
 {
     public ParticleSystem BloodParticles;
+    public Animator Anim;
+
 
     public void Explode()
     {
+        Anim.SetTrigger("Die");
         var particles = Instantiate(BloodParticles, transform.position, Quaternion.identity);
         Destroy(particles.gameObject, 10.0f);
+    }
+    public void Despawn()
+    {
         Destroy(gameObject);
     }
 }
