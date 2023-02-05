a3211b83f2c51da7526108a7c0ec8e0e2654229d
laurirasanen <lauri@rasanen.dev>
2023-02-05 10:35:06 +0200

    Gameplay text

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,23 +10,46 @@ public class GameManager : MonoBehaviour
     public TMPro.TextMeshProUGUI HumansText;
 
     private List<Human> humans;
+    private bool killedOne;
+    private int startingCount;
 
     void Start()
     {
-        humans = Object.FindObjectsOfType<Human>().ToList();    
+        humans = Object.FindObjectsOfType<Human>().ToList();
+        startingCount = humans.Count;
     }
 
     void Update()
     {
         humans = humans.Where(h => h != null && !h.IsDead).ToList();
 
-        if (humans.Count > 0)
+        if (humans.Count < startingCount)
         {
-            HumansText.text = $"{humans.Count} knights remain!";
+            killedOne = true;
+        }
+
+        if (!killedOne)
+        {
+            HumansText.text = $"Go forth";
         }
         else
         {
-            HumansText.text = "";
+            if (humans.Count > 10)
+            {
+                HumansText.text = $"Slay them all!";
+            }
+            else if (humans.Count > 1)
+            {
+                HumansText.text = $"{humans.Count} knights remain!";
+            }
+            else if (humans.Count == 1)
+            {
+                HumansText.text = $"Leave no survivors!";
+            }
+            else
+            {
+                HumansText.text = "You have done well";
+            }
         }
     }
 }
